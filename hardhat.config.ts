d2915e5ab294124c470a119d8617ce3a5411902d
Cristian Perez <cristian.ap84@gmail.com>
2023-01-20 16:35:01 -0300

    WIP.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from 'hardhat/config'
-import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-ethers'
+import '@nomicfoundation/hardhat-chai-matchers'
 import '@nomicfoundation/hardhat-toolbox'
 
 const config: HardhatUserConfig = {
